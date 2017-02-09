daf3f16eebe1cabe8f7195f60bf1844e9b458518
梅毅 <my@jovip-wndib6xtb.pianodisc.com>
2017-02-09 11:10:51 +0800

    设置启动页面和app Icon

--- 新闻阅读器/新闻阅读器/Controller/Page/MyBasePage.h
+++ 新闻阅读器/新闻阅读器/Controller/Page/MyBasePage.h
@@ -1,15 +1,13 @@
 //
-//  ViewController.h
+//  MyBasePage.h
 //  新闻阅读器
 //
 //  Created by 梅毅 on 2017/2/9.
 //  Copyright © 2017年 my. All rights reserved.
 //
 
-#import "MyBasePage.h"
+#import <UIKit/UIKit.h>
 
-@interface HomePage : MyBasePage
-
+@interface MyBasePage : UIViewController
 
 @end
-
---
+++ 新闻阅读器/新闻阅读器/Controller/Page/MyBasePage.m
@@ -0,0 +1,28 @@
+//
+//  MyBasePage.m
+//  新闻阅读器
+//
+//  Created by 梅毅 on 2017/2/9.
+//  Copyright © 2017年 my. All rights reserved.
+//
+
+#import "MyBasePage.h"
+
+@interface MyBasePage ()
+
+@end
+
+@implementation MyBasePage
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+
+@end
